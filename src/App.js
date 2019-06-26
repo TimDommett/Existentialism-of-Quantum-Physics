637edc68315bcbe82276cb0db157441e6edc1566
Tim Dommett <timdom11@gmail.com>
2019-06-26 13:36:17 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Existentialism of Quantum Physics</h1>
+        <p>
+          Quantum Physics describe the fundamental rules of the universe that
+          enable the mechanisms for the entire universe, range from atoms to
+          galaxies, to exist and even for you to read these words. EQP was
+          created with two aims: 1. To teach anyone about quantum physics, the
+          building blocks of our universe, and to get more students interested
+          in the field. 2. To test a simulation theory concept - if we can
+          simulate the fundamental rules of the universe, we will be able to
+          simulate anything in the universe using software. Simulation theory
+          brings up many philosophical and ethical conundrums, including
+          whether we are currently in a simulation and if we could ever know.
+          Building a virtual “reality” if perfectly model the standard model
+          then increase in size to the earth then essentially created exactly
+          what is us
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
